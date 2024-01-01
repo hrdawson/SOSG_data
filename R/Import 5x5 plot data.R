1d27daef45e8463e166a3acbb91c802887d13161
hrdawson <hrosedawson@gmail.com>
2024-01-02 09:50:17 +1100

    Mutated non-numerics to numeric-compatible values

--- R/Import 5x5 plot data.R
+++ R/Import 5x5 plot data.R
@@ -11,7 +11,20 @@ data = read_excel("raw_data/fixed area plot (permanent plot) data/Plot 01.xlsx",
                   range = cell_cols("A:J")) |>
   row_to_names(row_number = 1) |>
   clean_names(case = "lower_camel") |>
-  drop_na(plot)
+  drop_na(plot) |>
   # Convert non-numeric values to numeric
   # For less than 5% cover, replace value with trace amount number
-
+  mutate(coverPercent = str_replace(coverPercent, "<5", "0.1"),
+         # Abundance is trickier because it's a large size class
+         # IE <1000 means between 500 and 1000
+         # But running on the idea of relative proportion
+         abundance = case_when(
+           abundance == "<100" ~ "100",
+           abundance == "<1000" ~ "999",
+           abundance == ">1000" ~ "1000",
+           abundance == "<20" ~ "20",
+           abundance == "<5" ~ "5",
+           abundance == "<50" ~ "50",
+           abundance == "<500" ~ "500",
+           TRUE ~ abundance
+         ))
