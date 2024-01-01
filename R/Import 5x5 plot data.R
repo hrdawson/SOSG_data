ab3e990ed481fe8ef93f9331b850f6159b3b5644
hrdawson <hrosedawson@gmail.com>
2024-01-02 10:08:05 +1100

    Format change

--- R/Import 5x5 plot data.R
+++ R/Import 5x5 plot data.R
@@ -1,16 +1,15 @@
 # 5x5 Plot data manager
-
-# First, build a way to read in one of these sheets before tackling all of them
 library(readxl)
 library(janitor)
 library(openxlsx)
 library(lubridate)
 library(tidyverse)
 library(tidylog)
+# First, build a way to read in one of these sheets before tackling all of them
 
 subplot.data = read_excel("raw_data/fixed area plot (permanent plot) data/Plot 01.xlsx",
                   sheet = "5 x 5 subplots", skip = 1,
-                  range = cell_cols("A:J"), col_types = ) |>
+                  range = cell_cols("A:J")) |>
   row_to_names(row_number = 1) |>
   clean_names(case = "lower_camel") |>
   drop_na(plot) |>
