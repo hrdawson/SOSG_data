198a9f03e1e2ebbf3c41ecc386e0c73d8b5c4bbe
hrdawson <hrosedawson@gmail.com>
2024-01-02 17:09:22 +1100

    Added scat section

--- R/Import 1x1 plot data.R
+++ R/Import 1x1 plot data.R
@@ -1,3 +1,8 @@
+library(tidyverse)
+library(tidylog)
+
+source("R/functions/one_to_letter.R")
+
 # Read in files ----
 # Make file list
 filesCover <- dir(path = "raw_data/fixed area plot (permanent plot) data", pattern = ".xlsx",
@@ -57,3 +62,42 @@ cover.data = tempCover |>
          date = openxlsx::convertToDate(date))
 
 # write.csv(cover.data, "clean_data/1x1 cover data.csv")
+
+# Scat data ----
+# Need to build the headers separately ----
+scat.data.header = tempCover |>
+  # Select just the cover columns + meta columns
+  select('...1':'...23') |>
+  # Select just the headers
+  slice_head(n = 3) |>
+  # Fill in the NA cells
+  fill('...1':'...23', .direction = "down") |>
+  # Make just the one row of headers
+  slice_tail(n = 1)
+
+## Clean the data ----
+scat.data = tempCover |>
+  # Select just the cover columns + meta columns
+  select('...1':'...23') |>
+  # Remove the existing header rows
+  slice(4:n()) |>
+  # Add in correct headers and assign
+  add_row(scat.data.header, .before = 1) |>
+  row_to_names(row_number = 1) |>
+  clean_names(case = "lower_camel") |>
+  # Clean the data
+  # Some data entry was not completed
+  # THIS STEP IS DANGEROUS, check here first for errors
+  fill(plot, .direction = "down") |>
+  # Now we can group by plot
+  group_by(plot) |>
+  fill(assessor1:date, .direction = "down") |>
+  ungroup() |>
+  filter(plot != "Plot") |>
+  # Tidy the data
+  one_to_letter_plot(c(f:a)) |> rename(equidae = values) |>
+  one_to_letter_plot(c(f_2:a_2)) |> rename(cervidae = values) |>
+  one_to_letter_plot(c(f_3:a_3)) |> rename(leporidae = values) |>
+  one_to_letter_plot(c(f_4:a_4)) |> rename(suidae = values) |>
+  one_to_letter_plot(c(f_5:a_5)) |> rename(macropodidae = values) |>
+  one_to_letter_plot(c(f_6:a_6)) |> rename(vombatidae = values)
