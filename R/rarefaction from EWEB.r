d9596911cce6c8c59b40e74ded4a5214b71becd9
hrdawson <hrosedawson@gmail.com>
2024-02-12 14:48:00 +1100

    Modified for this project and to tidy

--- R/rarefaction from EWEB.r
+++ R/rarefaction from EWEB.r
@@ -2,70 +2,45 @@ library(vegan)
 library(Hotelling)
 library(readxl)
 
-data = as.data.frame(read_excel("tables/2020.08.24.raresample.xlsx"), sheet = "rare")
-#colnames(data) = data[1,]
-rownames(data) = data[,1]
-data$plotid = NULL
-
-head(data)
-
-#Rarefaction
-sp.abund <- rowSums(data)  #gives the number of individuals found in each plot
-raremax <- min(rowSums(data))  #rarefaction uses the smallest number of observations (individuals here) per sample to extrapolate the expected number if all other samples only had that number of observations
-raremax
-Srare <- rarefy(data, raremax)
-par(mfrow = c(1, 1))
-plot(sp.abund, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
-rarecurve(data, col = "blue")
-
-#Code from Alison----
-#get richness estimators (for each sample, cumulative)
-pool.aurora <- poolaccum(data)
-#plot all: obs richness and  estimators
-plot(pool.aurora)
-
-#build the species accumulation curve & rarefaction curve (expected)
-aurora.specaccum <- specaccum(data,method = "rarefaction")
-#plot the curve with some predefined settings
-plot(aurora.specaccum,ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
-
-#build a expected curve (randomization for boxplot comparison)
-aurora.specaccum.rand <- specaccum(data, "random")
-#plot both curves ("observed" vs "randomized")
-plot(aurora.specaccum,ci.type="poly", col="blue", lwd=2, ci.lty=0, ci.col="lightblue")
-boxplot(aurora.specaccum.rand, col="yellow", add=TRUE, pch="+")
-
-library(iNEXT)
-library(readxl)
-
-incid = list()
-
-incid[[1]] = as.data.frame(read_excel("tables/2020.08.25.raresample.xlsx", sheet = "field1"))
-incid[[2]] = as.data.frame(read_excel("tables/2020.08.25.raresample.xlsx", sheet = "field2"))
-incid[[3]] = as.data.frame(read_excel("tables/2020.08.25.raresample.xlsx", sheet = "field3"))
-incid[[4]] = as.data.frame(read_excel("tables/2020.08.25.raresample.xlsx", sheet = "water1"))
-incid[[5]] = as.data.frame(read_excel("tables/2020.08.25.raresample.xlsx", sheet = "water2"))
-
-names(incid) = c("field1", "field2", "field3", "water1")
-
-rownames(incid[["field1"]]) = incid[["field1"]][["plot"]]; incid[["field1"]][["plot"]] = NULL
-head(incid[["field1"]])
-
-rownames(incid[["field2"]]) = incid[["field2"]][["plot"]]; incid[["field2"]][["plot"]] = NULL
-rownames(incid[["field3"]]) = incid[["field3"]][["plot"]]; incid[["field3"]][["plot"]] = NULL
-rownames(incid[["water1"]]) = incid[["water1"]][["plot"]]; incid[["water1"]][["plot"]] = NULL
-rownames(incid[["water2"]]) = incid[["water2"]][["plot"]]; incid[["water2"]][["plot"]] = NULL
-
 ##This is the iNEXT that actually works ----
+# Format the data for subplot analysis ----
+subplot.raw = read.csv("clean_data/5x5 subplot data.csv") |>
+  select(plot, midlineDistance, species, coverPercent) |>
+  # Create unique subplot names
+  mutate(subplot = paste0(plot, ".", midlineDistance)) |>
+  # Duplicate species in some subplots
+  # But we don't care what value they have for these analyses
+  mutate(value = case_when(
+    coverPercent > 0 ~ '1',
+    TRUE ~ NA
+  )) |>
+  select(-coverPercent) |>
+  distinct() |>
+  mutate(value = as.numeric(str_trim(value))) |>
+  # Pivot species
+  pivot_wider(names_from = species, values_from = value) |>
+  select(-c(plot, midlineDistance)) |>
+  # Transpose
+  t() |>
+  janitor::row_to_names(1) %>%
+  # Turn binary
+  replace(is.na(.), 0)
+
 incid = list()
 
-incid[[1]] = as.data.frame(read_excel("tables/subplotspecies_master.xlsx", sheet = "field.incid"))
-incid[[2]] = as.data.frame(read_excel("tables/subplotspecies_master.xlsx", sheet = "water.incid"))
-incid[[3]] = as.data.frame(read_excel("tables/subplotspecies_master.xlsx", sheet = "forest.incid"))
-names(incid) = c("field", "riparian", "forest")
-rownames(incid[["field"]]) = incid[["field"]][["plot"]]; incid[["field"]][["species"]] = NULL
-rownames(incid[["riparian"]]) = incid[["riparian"]][["plot"]]; incid[["riparian"]][["species"]] = NULL
-rownames(incid[["forest"]]) = incid[["forest"]][["plot"]]; incid[["forest"]][["species"]] = NULL
+#Low dieback severity
+incid[[1]] = as.data.frame(subplot.raw) |> select('1.10':'10.40') |> mutate_if(is.character,as.numeric)
+
+# Moderate dieback severity
+incid[[2]] = as.data.frame(subplot.raw) |> select('11.10':'12.40', '17.10':'18.40') |> mutate_if(is.character,as.numeric)
+
+# High dieback severity
+incid[[3]] = as.data.frame(subplot.raw) |> select('13.10':'16.40', '19.10':'20.40') |> mutate_if(is.character,as.numeric)
+
+names(incid) = c("low", "moderate", "severe")
+rownames(incid[["low"]]) = incid[["low"]][["plot"]]; incid[["low"]][["species"]] = NULL
+rownames(incid[["moderate"]]) = incid[["moderate"]][["plot"]]; incid[["moderate"]][["species"]] = NULL
+rownames(incid[["severe"]]) = incid[["severe"]][["plot"]]; incid[["severe"]][["species"]] = NULL
 
 rare.inext = iNEXT(incid, datatype = "incidence_raw", endpoint = 56)
 
@@ -83,41 +58,43 @@ ggiNEXT(rare.inext, type = 2)+
   theme(legend.position = "bottom",panel.border = element_rect(colour = "black", fill=NA))
 dev.off()
 
+# Format the data for plot analysis ----
+plot.raw = read.csv("clean_data/5x5 subplot data.csv") |>
+  select(plot, species, coverPercent) |>
+  # Duplicate species in some subplots
+  # But we don't care what value they have for these analyses
+  mutate(value = case_when(
+    coverPercent > 0 ~ '1',
+    TRUE ~ NA
+  ),
+  plot = str_trim(plot)) |>
+  select(-coverPercent) |>
+  distinct() |>
+  # Pivot species
+  pivot_wider(names_from = species, values_from = value) |>
+  # Transpose
+  t() |>
+  janitor::row_to_names(1) %>%
+  # Turn binary
+  replace(is.na(.), 0)
+
 incid = list()
-incid[[1]] = c(13, 4,7,4,9,8,11,10,9,16,9,12,7,7) #field 1
-incid[[2]] = c(11, 5, 7, 11,9, 15,15,14,6,10,9,9) #field 2
-incid[[3]] = c(4, 6,6,13,11) #field 3
-incid[[4]] = c(7, 18,26,27,20,18,29,9) #water 1
 
+#Low dieback severity
+incid[[1]] = as.data.frame(plot.raw) |> select('1':'10') |> mutate_if(is.character,as.numeric)
 
-names(incid) = c("field1", "field2", "field3", "water1", "water2")
+# Moderate dieback severity
+incid[[2]] = as.data.frame(plot.raw) |> select('11':'12', '17':'18') |> mutate_if(is.character,as.numeric)
 
-data.t = t(data)
-head(data.t)
+# High dieback severity
+incid[[3]] = as.data.frame(plot.raw) |> select('13':'16', '19':'20') |> mutate_if(is.character,as.numeric)
 
-#Sum abundances for the aurora site; i.e. consider the aurora data as a single site with spp abundances (summed over the sampling days)
-aurora.sum <- rowSums(data.t)
+names(incid) = c("low", "moderate", "severe")
+rownames(incid[["low"]]) = incid[["low"]][["plot"]]; incid[["low"]][["species"]] = NULL
+rownames(incid[["moderate"]]) = incid[["moderate"]][["plot"]]; incid[["moderate"]][["species"]] = NULL
+rownames(incid[["severe"]]) = incid[["severe"]][["plot"]]; incid[["severe"]][["species"]] = NULL
 
-#apply `iNEXT` main function
-aurora.sum.inext <- iNEXT(incid, q = 0, datatype = "incidence_freq")
-#look at the data
-aurora.sum.inext
-#plot the results
-ggiNEXT(aurora.sum.inext)
-
-#iNEXT Quadrat Abundance ----
-quad = list()
-
-quad[[1]] = as.data.frame(read_excel("tables/quadrats_master.xlsx", sheet = "relativecover.field"))
-quad[[2]] = as.data.frame(read_excel("tables/quadrats_master.xlsx", sheet = "relativecover.water"))
-quad[[3]] = as.data.frame(read_excel("tables/quadrats_master.xlsx", sheet = "relativecover.forest"))
-names(quad) = c("field", "water", "forest")
-rownames(quad[["field"]]) = quad[["field"]][["plot"]]; quad[["field"]][["species"]] = NULL
-rownames(quad[["water"]]) = quad[["water"]][["plot"]]; quad[["water"]][["species"]] = NULL
-rownames(quad[["forest"]]) = quad[["forest"]][["plot"]]; quad[["forest"]][["species"]] = NULL
-
-quad.inext = iNEXT(quad)
-ggiNEXT(quad.inext)
+rare.inext = iNEXT(incid, datatype = "incidence_raw", endpoint = 40)
 
 png("inext.speciesdiversity.png", width = 8, height = 6, units = "in", res = 300)
 ggiNEXT(rare.inext, type = 1)+
