f6ddf06a06bd523895976a30b2a6e15fe7348b69
hrdawson <hrosedawson@gmail.com>
2024-03-28 12:20:39 +1100

    Moved GIS code to separate script

--- R/Canopy health classes.R
+++ R/Canopy health classes.R
@@ -149,48 +149,6 @@ tree.data.all.canopy = tree.data.canopy |>
 
 # write.csv(tree.data.all.canopy, "outputs/2024.03.27_Permanent plot trees for Weerach.csv")
 
-# Export dendros and GPS points ----
-gps.extra = read.csv("raw_data/SOSG dendro GPS points added on.csv") |> rename(GPS2 = GPS) |>
-  select(dendroNr, GPS2)
-
-tree.data.gps = tree.data.all.canopy |>
-  select(plotNickname, plot, tree, stem, dbh, GPS, dendroNr, dendroType) |>
-  # Resolve funny formatting of dendroNr
-  mutate(dendroNr = case_when(
-    dendroNr == "15?" ~ 15,
-    TRUE ~ dendroNr),
-    dendroNr = round(as.numeric(dendroNr), 1),
-    GPS = as.integer(GPS)) |>
-  # Select trees with dendros
-  drop_na(dendroNr) |>
-  filter(dendroType != "none") |>
-  distinct() |>
-  # Add in missing points
-  left_join(gps.extra)  |>
-  # Add in plot nicknames
-  mutate(plotNickname = case_when(
-    plot == 7.2 ~ "Spencers Creek",
-    plot == 21 ~ "Illawong",
-    plot == 22 ~ "Pipers Gap",
-    TRUE ~ plotNickname
-  )) |>
-  # Bring together the two GPS columns
-  mutate(GPSpoint = coalesce(GPS, GPS2)) |>
-  arrange(dendroNr)
-
-# write.csv(tree.data.gps, "outputs/2024.03.28_dendroGPSPoints.csv")
-
-## Find missing dendros ----
-test = tree.data.all.canopy |>
-  select(plotNickname, plot, tree, stem, dbh, GPS, dendroNr, dendroType) |>
-  # Select trees with dendros
-  drop_na(dendroNr) |>
-  filter(dendroType != "none") |>
-  distinct()
-
-missing.dendro = gps.extra |>
-  anti_join(test)
-
 # Figure out basal area for all plots ----
 
 tree.data.BA.sum = tree.data.all.canopy |>
